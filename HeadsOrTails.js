74316ccd3626a4de208eda4bef04522fa7c9f4f2
Subhamdas20 <Subham21.sd@gmail.com>
2021-11-30 18:10:50 +0530

    Added : 5 programs

---
+++ HeadsOrTails.js
@@ -0,0 +1,4 @@
+let value = (Math.floor(Math.random() * 10) % 2); 
+if(value==0){
+console.log("Heads " );}
+else console.log("tails");
---
+++ LeapYear.js
@@ -0,0 +1,11 @@
+let year = 2000;
+checkLeapYear(year);
+function checkLeapYear(year){
+
+    
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
